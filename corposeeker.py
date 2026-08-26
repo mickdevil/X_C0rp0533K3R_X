e95a63f255739a303b6174af3cc09c5e181bb25a
mickdevil <navasiolau.mikalai@gmail.com>
2026-08-26 13:49:12 +0200

    alpha version, the script update the page dynamicly and it's good ;)

--- corposeeker.py
+++ corposeeker.py
@@ -52,14 +52,83 @@ geodb = duckdb.read_parquet(GEOLOC)
 
 @app.route('/')
 def index():
-    return render_template('index.html', amount=0)
+    return render_template('td.html', amount=0)
+
+@app.route('/td', methods = ['POST', 'GET'])
+def td():
+    if request.method == 'GET' :
+        sql_query = "\
+        SELECT \
+        e.siren, \
+        e.siret, \
+        u.denominationUniteLegale, \
+        e.etatAdministratifEtablissement, \
+        e.etablissementSiege, \
+        e.dateCreationEtablissement, \
+        e.activitePrincipaleEtablissement, \
+        e.activitePrincipaleNAF25Etablissement, \
+        e.trancheEffectifsEtablissement, \
+        e.codePostalEtablissement, \
+        e.codeCommuneEtablissement, \
+        g.y_latitude as lat, \
+        g.x_longitude as lng \
+        FROM buildingsdb AS e \
+        JOIN corposdb AS u ON e.siren = u.siren \
+        JOIN geodb as g ON e.siret = g.siret \
+        WHERE \
+        "
+        
+        #chekboxes, there is no guarantie u get them in the post
+        is_hq = request.args.get('is_hq') #chekbox true or none //done
+        ignore_hq = request.args.get('ignore_hq') #chekbox true or none
+        ignore_date = request.args.get('ignore_date') #chekbox true or none
+        ignore_size = request.args.get('ignore_size') #chekbox true or none
+        ignore_comune = request.args.get('ignore_comune') #chekbox true or none
+        #there is guarantie you get it
+        status = request.args.get('status') #A for active, F for dead //done
+        date_creation_from = request.args.get('date_creation_from') #the data when they started hustle //done
+        trancheEffectifs = request.args.get('trancheEffectifs') #how many slaves they have //done
+        postal_code = request.args.get('postal_code') #where they are 
+        comune_code = request.args.get('comune_code') #where exactly they are //done
+        main_acctivity = request.args.get('main_acctivity') #what they do
+        
+        #all posible ignores
+        if not ignore_hq :
+            if is_hq :
+                sql_query += " e.etablissementSiege = true AND "
+            else :
+                sql_query += " e.etablissementSiege = false AND "
+        if not ignore_date :
+            sql_query += f" e.dateCreationEtablissement >= {date_creation_from} AND " 
+        if not ignore_size :
+            sql_query += f" e.trancheEffectifsEtablissement >= '{trancheEffectifs}' AND e.trancheEffectifsEtablissement != 'NN' AND"
+        if not ignore_comune :
+            sql_query += f" e.codeCommuneEtablissement LIKE '{comune_code}' AND "
+            #guaranied stuff
+        sql_query += f" e.etatAdministratifEtablissement = '{status}' AND "
+        sql_query += f" e.codePostalEtablissement LIKE '{postal_code}' AND "
+        sql_query += f" (e.activitePrincipaleEtablissement LIKE '{main_acctivity}' OR e.activitePrincipaleNAF25Etablissement LIKE '{main_acctivity}')"
+        print(sql_query)
+        corpos = duckdb.sql(sql_query).df().to_dict('records')
+        if corpos :
+            for corpo in corpos :
+                try :
+                    corpo["trancheEffectifsEtablissement"] = tecodes[corpo["trancheEffectifsEtablissement"]]
+                except :
+                    ...
+                try :
+                    corpo["main_acctivity"] = naf_codes_dict[corpo["activitePrincipaleNAF25Etablissement"]]
+                except :
+                    try :
+                        corpo["main_acctivity"] = naf_codes_dict[corpo["activitePrincipaleEtablissement"]]
+                    except :
+                        corpo["main_acctivity"] = "name not found, go google the NAF code bro"
+                        print(corpos[0])
+        return render_template('results.json', corpos=corpos)
+                                            
 
 @app.route('/search', methods = ['POST', 'GET'])
 def search():
-    """
-    should get : NAF25 NAF_old 
-    
-    """
     sql_query = "\
     SELECT \
     e.siren, \
--- static/script.js
+++ static/script.js
@@ -1,1 +1,115 @@
-//alert(1)
+document.getElementById("search_btn").addEventListener('click', async function (e) {
+    e.preventDefault();
+    const search_form = document.getElementById("search_form");
+    const form_data = new FormData(search_form);
+    const daReq = new URLSearchParams(form_data).toString();
+        try {
+        const response = await fetch('/td?' + daReq, {
+            method: 'GET'
+        });
+
+        if (!response.ok) throw new Error('Request failed');
+        const data = await response.json();
+        corpos_root = document.getElementById("allDaCorposInDaArea");
+        corpos_root.inerHTML = '';
+        corpos_root.appendChild(mkCorposLst(data));
+    }
+    catch (error) {
+        console.error('Fetch error:', error);
+        alert('Error: ' + error.message);
+    }
+})
+
+//results_lst_root the ol i use as root element
+
+function mkCorposLst(data){
+    console.log(data);
+    const fragment = document.createDocumentFragment();
+
+    data.forEach(corpo => {
+        const li = document.createElement('li');
+        const res_div = document.createElement('div');
+        res_div.className = "result";
+        li.appendChild(res_div);
+        
+        const name = document.createElement('p');
+        name.className = "company_name";
+        name.textContent = 'COMPANY NAME : ' + (corpo.denominationUniteLegale || 'no name for that corpo, fuck INSEE');
+        res_div.appendChild(name);
+
+        const res_div_flx = document.createElement('div');
+        res_div_flx.className = "result-flx";
+        res_div.appendChild(res_div_flx);
+
+        const res_div_lft = document.createElement('div');
+        res_div_lft.className = "result-left";
+        res_div_flx.appendChild(res_div_lft);
+
+        const acctivity = document.createElement('p');
+        acctivity.className = "result_field";
+        acctivity.textContent = 'acctivity : ' + corpo.main_acctivity;
+        res_div_lft.appendChild(acctivity);
+//////////
+
+        const naf25 = document.createElement('p');
+        naf25.className = "result_field";
+        naf25.textContent = 'NAF25 : ' + corpo.activitePrincipaleNAF25Etablissement;
+        res_div_lft.appendChild(naf25);
+
+        const naf_old = document.createElement('p');
+        naf_old.className = "result_field";
+        naf_old.textContent = 'NAF OLD : ' + corpo.activitePrincipaleEtablissement;
+        res_div_lft.appendChild(naf_old);
+
+        const slavesAmount = document.createElement('p');
+        slavesAmount.className = "result_field";
+        slavesAmount.textContent = 'number of emplyes : ' + corpo.trancheEffectifsEtablissement;
+        res_div_lft.appendChild(slavesAmount);
+
+        const postal_code = document.createElement('p');
+        postal_code.className = "result_field";
+        postal_code.textContent = 'POSTAL CODE : ' + corpo.codePostalEtablissement;
+        res_div_lft.appendChild(postal_code);
+
+        const comune_code = document.createElement('p');
+        comune_code.className = "result_field";
+        comune_code.textContent = 'CUMUNE CODE : ' + corpo.codeCommuneEtablissement;
+        res_div_lft.appendChild(comune_code);
+
+        ///right side & right part div
+        const res_div_right = document.createElement('div');
+        res_div_right.className = "result-right";
+        res_div_flx.appendChild(res_div_right);
+
+        const is_active = document.createElement('p');
+        is_active.className = "result_field";
+        is_active.textContent = 'IS ACTIVE : ' + (corpo.etatAdministratifEtablissement == 'A' ? 'YES' : 'NO'); 
+        res_div_right.appendChild(is_active);
+
+        const is_hq = document.createElement('p');
+        is_hq.className = "result_field";
+        is_hq.textContent = 'IS HQ : ' + corpo.etablissementSiege;
+        res_div_right.appendChild(is_hq);
+
+        const siren = document.createElement('p');
+        siren.className = "result_field";
+        siren.textContent = 'SIREN :  ' + corpo.siren;
+        res_div_right.appendChild(siren);
+
+        const siret = document.createElement('p');
+        siret.className = "result_field";
+        siret.textContent = 'SIRET :  ' + corpo.siret;
+        res_div_right.appendChild(siret);
+
+
+        const cords = document.createElement('a');
+        cords.className = "result_field";
+        cords.href = "https://www.google.com/maps?q=" + corpo.lat + ',' + corpo.lng;
+        cords.target = "_blank";
+        cords.textContent = '' + corpo.lat + ',' + corpo.lng;
+        res_div_right.appendChild(cords);
+
+        fragment.appendChild(li);
+    });
+    return fragment;
+}
